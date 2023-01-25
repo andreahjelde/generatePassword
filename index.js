cd244131815b88243b79bdb2f4fbbacfa682544c
Andrea <s364756@oslomet.no>
2023-01-25 23:17:36 +0100

    La til input og knapp som kopierer tekst

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 let password1El = document.getElementById("password1-el")
 let password2El = document.getElementById("password2-el")
 
-function generatePassword() {
-
-    let randomIndex = Math.floor(Math.random() * characters.length)
-    for (let i = 0; i < characters.length; i++) {
-        let random = characters[randomIndex]
-        password1El.textContent = random
+function generatePassword(lengde) {
+    let passord = ""
+    for (let i = 0; i < lengde; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        passord += characters[randomIndex]
     }
+    return passord
+}
 
+function generatePasswordHandler() {
+    let input = document.getElementById("input").value;
+    password1El.textContent = generatePassword(input);
+    password2El.textContent = generatePassword(input);
+}
 
+function copyPassword1Handler() {
+    navigator.clipboard.writeText(document.getElementById("password1-el").innerHTML)
+    console.log(document.getElementById("password1-el").innerHTML)
+    alert("Password copied")
 }
 
 
+function copyPassword2Handler() {
+    navigator.clipboard.writeText(document.getElementById("password1-el").innerHTML)
+    alert("Password copied")
+    console.log(document.getElementById("password1-el").innerHTML)
+}
+
+//Inputfelt som skal ta inn et tall, og det tallet skal være tallet til passordet
+//print ut tallet som er skrevet inn i input
+
+//knapp så de kan kopiere ut passordet med  --> clipboard
